7614c307fd31ea9b95035e481fdbe4bbb4ffbb4b
Salman <charliekhan7410@gmail.com>
2022-08-03 17:35:08 +0530

    Register Language Selection Completed

--- src/components/login-components/Signup.jsx
+++ src/components/login-components/Signup.jsx
@@ -41,6 +41,8 @@ const Signup = () => {
         else {
             setUser({ ...user, mobile: `${userData.code} ${userData.mobile}` })
             user.mobile = `${userData.code} ${userData.mobile}`;
+            console.log(user)
+            console.log(userData)
             axios.post('https://apiserver.msgmee.com/public/sendOtp', user)
                 .then((res) => {
                     console.log(res.data.data)
@@ -142,8 +144,13 @@ const Signup = () => {
                                                 <p className="notimsg-blk">When you will click on continue,  you will receive a verification code on the mobile number that you have entered.</p>
                                                 <div className="btn-section">
                                                     <Stack spacing={2} sx={{ width: '100%' }} id="stack">
-                                                        <button className="btn btn-solid btn-lg">CONTINUE</button>
-                                                        {/* Snackbar */}
+                                                        {
+                                                            userData.mobile ? (
+                                                                <button className={"btn btn-solid btn-lg"} >CONTINUE</button>
+                                                            ) : (
+                                                                <button className={"btn btn-solid btn-lg"} disabled>CONTINUE</button>
+                                                            )
+                                                        }
                                                         <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={open} autoHideDuration={1500} onClose={handleClose}>
                                                             <Alert onClose={handleClose} severity={alert.sev} sx={{ width: '100%' }}>
                                                                 {alert.content}
@@ -162,24 +169,10 @@ const Signup = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="language-selection-blk">
-                                <div className="langauge-block">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 14 14" fill="none" className="iw-14 globe-svg">
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M7.00004 0.583344C3.44171 0.583344 0.583374 3.44168 0.583374 7.00001C0.583374 10.5583 3.44171 13.4167 7.00004 13.4167C10.5584 13.4167 13.4167 10.5583 13.4167 7.00001C13.4167 3.44168 10.5584 0.583344 7.00004 0.583344ZM12.1917 6.41668H9.85837C9.74171 4.78334 9.21671 3.26668 8.28337 1.92501C10.3834 2.45001 11.9584 4.25834 12.1917 6.41668ZM8.75004 7.58334H5.30837C5.42504 9.15834 6.00837 10.675 7.05837 11.9C7.99171 10.675 8.57504 9.15834 8.75004 7.58334ZM5.30837 6.41668C5.48337 4.84168 6.06671 3.32501 7.00004 2.10001C7.99171 3.38334 8.57504 4.90001 8.69171 6.41668H5.30837ZM4.14171 6.41668C4.25837 4.78334 4.78337 3.26668 5.65837 1.92501C3.61671 2.45001 2.04171 4.25834 1.80837 6.41668H4.14171ZM1.80837 7.58334H4.14171C4.25837 9.21668 4.78337 10.7333 5.71671 12.075C3.61671 11.55 2.04171 9.74168 1.80837 7.58334ZM9.91671 7.58334C9.74171 9.21668 9.21671 10.7333 8.34171 12.075C10.3834 11.55 11.9584 9.74168 12.25 7.58334H9.91671Z" fill="#647589"></path>
-                                    </svg>
-                                    <select className="form-select" value={user.languagId} onChange={(e) => setUser({ ...user, languagId: e.target.value })}>
-                                        <option value="">Select Language</option>
-                                        {
-                                            language.filter(lan => lan.isActive === 1).map((lang) => {
-                                                return <option value={lang.id}>{lang.name}</option>
-                                            })
-                                        }
-                                    </select>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
-                    <LoginLanguage></LoginLanguage>
+                    <LoginLanguage setUser={user}></LoginLanguage>
                 </div>
             </section>
         </>
